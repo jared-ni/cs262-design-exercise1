f65cb2bc1fc3b1db82ce893fcc56870011a8291a
Bryan Han <bryan.han77@gmail.com>
2023-02-21 15:19:13 -0500

    finalized tests for rpc

--- test_client.py
+++ test_client.py
@@ -27,21 +27,14 @@ CLIENT_KEY = b'cs262IsFunAndWaldoIsCool'
 
 class TestClient(TestCase):
 
-    # @mock.patch('client.input', create=True)
-    # def test_client(self, mocked_input):
-    #     mocked_input.side_effect = ["yes", "yes", "user1", "1", "1", "yes", "user1", "1", "./list", "./disconnect"]
-    #     result = client.start()
-    #     print("test")
-
-    # there are two accounts for testing purposes: user1 has password 1, user2 has password 2
-
     @mock.patch('client.input', create=True)
     def test_register1(self, mocked_input):
         client_test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_test.connect((socket.gethostbyname(socket.gethostname()), 48789))
 
         mocked_input.side_effect = ["yes", "user1", "1", "1"]
-        result = client.register_user(client_test)
+        client.register_user(client_test)
+        result = client.listen_from_server(client_test, [True])
         client_test.close()
         self.assertEqual(result, True)
 
@@ -61,7 +54,8 @@ class TestClient(TestCase):
         client_test.connect((socket.gethostbyname(socket.gethostname()), 48789))
 
         mocked_input.side_effect = ["yes", "user1", "1"]
-        result = client.login_user(client_test)
+        client.login_user(client_test)
+        result = client.listen_from_server(client_test, [True])
         client_test.close()
         self.assertEqual(result, True)
 
@@ -71,21 +65,11 @@ class TestClient(TestCase):
         client_test.connect((socket.gethostbyname(socket.gethostname()), 48789))
 
         mocked_input.side_effect = ["yes", "user1", "1", "no"]
-        result = client.login_user(client_test)
-        result = client.login_user(client_test)
+        client.login_user(client_test)
+        client.login_user(client_test)
+        result = client.listen_from_server(client_test, [True])
         client_test.close()
-        self.assertEqual(result, False)
-
-    # @mock.patch('client.input', create=True)
-    # def test_login3(self, mocked_input):
-    #     client_test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client_test.connect((socket.gethostbyname(socket.gethostname()), 48789))
-
-    #     hashed = client.get_hashed_password("1")
-    #     # mocked_input.side_effect = ["no"]
-    #     result = client.send(client_test, hashed, LOGIN)
-    #     client_test.close()
-    #     self.assertEqual(result, True)
+        self.assertEqual(result, True)
 
     @mock.patch('client.input', create=True)
     def test_delete1(self, mocked_input):
@@ -138,41 +122,11 @@ class TestClient(TestCase):
         client_test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_test.connect((socket.gethostbyname(socket.gethostname()), 48789))
 
-        client.forced_disconnect(client_test)
-        result = client.listen_from_server(client_test, [True])
+        with self.assertRaises(SystemExit) as cm:
+            client.forced_disconnect(client_test)
         client_test.close()
-        self.assertEqual(result, False)
+        self.assertEqual(cm.exception.code, 0)
 
 if __name__ == '__main__':
     unittest.main()
 
-
-
-
-
-# import subprocess
-# import time
-# import os
-
-# exec(open('client.py').read()); print("yes")
-# time.sleep(1)
-# print("yes")
-
-
-# import unittest
-# import socket
-# import client
-
-# class testClient(unittest.TestCase):
-
-#     def setUp(self):
-#         client.start()
-#         # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         # client_socket.connect((socket.gethostbyname(socket.gethostname()), 48789))
-#         # client_socket.send('message1'.encode())
-#         # print(client_socket)
-#         # self.assertEqual(client_socket.recv(1024).decode(), 'message1')
-#         # client_socket.close()
-		
-# if __name__ == '__main__':
-#     unittest.main()
